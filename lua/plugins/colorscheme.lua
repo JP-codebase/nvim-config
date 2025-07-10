6d7ea97f500afca2737a7ab9138310d2b33522db
JP <gianpaolosantarsiero@gmail.com>
2025-07-10 22:40:37 +0200

    Now works

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,7 +22,7 @@ return {
 		priority = 1000,
 		config = function()
 			require("rose-pine").setup({
-				variant = "auto", -- auto, main, moon, or dawn
+				variant = "moon", -- auto, main, moon, or dawn
 				dark_variant = "main", -- main, moon, or dawn
 				dim_inactive_windows = false,
 				extend_background_behind_borders = true,
@@ -36,8 +36,8 @@ return {
 				styles = {
 					bold = false,
 					italic = false,
-					transparency = true,
-					-- transparency = false,
+					-- transparency = true,
+					transparency = false,
 				},
 
 				groups = {
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,65 +1,74 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright", "clangd" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "pyright", "clangd" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.clangd.setup({
+      -- print("-> setting up clangd...")
+      lspconfig.clangd.setup({
 
-				-- on_attach = function(client, bufnr)
-				-- 	-- Disable clangd's formatting provider so that null-ls takes precedence.
-				-- 	client.server_capabilities.documentFormattingProvider = false
-				-- end,
+        -- on_attach = function(client, bufnr)
+        -- 	-- Disable clangd's formatting provider so that null-ls takes precedence.
+        -- 	client.server_capabilities.documentFormattingProvider = false
+        -- end,
 
-				capabilities = capabilities,
-				cmd = {
-					"clangd",
-					"--background-index",
-					"--clang-tidy",
-					"--header-insertion=iwyu",
-					"--header-insertion-decorators",
-					"--function-arg-placeholders",
-					"--completion-style=detailed",
 
-					"--fallback-style=LLVM",
-				},
-				init_options = {
-					fallbackFlags = { "--std=c++20" },
-				},
-			})
+        capabilities = capabilities,
+        cmd = {
+          "clangd",
+          -- "--compile-commands-dir=build",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--header-insertion-decorators",
+          "--function-arg-placeholders",
+          "--completion-style=detailed",
 
-			local opts = { noremap = true, silent = true }
+          "--fallback-style=LLVM",
+        },
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-			vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-		end,
-	},
+        root_dir = lspconfig.util.root_pattern(
+          ".",
+          ".git"
+        ),
+
+        init_options = {
+          fallbackFlags = { "--std=c++20" },
+        },
+      })
+
+      local opts = { noremap = true, silent = true }
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+      vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+    end,
+  },
 }
